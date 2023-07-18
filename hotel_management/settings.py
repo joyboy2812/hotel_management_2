e52a17ee38b512406c95f0efbe4644fb98854d44
joyboy2812 <tth281221@gmail.com>
2023-07-18 17:22:07 +0700

    Authenticate 1

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'hotels.apps.HotelsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- hotels/urls.py
+++ hotels/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home-page"),
     path('manage-hotel/', views.manageHotel, name="manage-hotel"),
     path('create-hotel/', views.createHotel, name="create-hotel"),
     path('update-hotel/<int:pk>/', views.updateHotel, name="update-hotel"),

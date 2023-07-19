56cbf202d7b35acb33aa697db7157dbf570dc17c
joyboy2812 <tth281221@gmail.com>
2023-07-19 23:19:52 +0700

    Add email password to .env

--- hotel_management/settings.py
+++ hotel_management/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+# Load variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -137,7 +141,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'tthieu281221@gmail.com'
 EMAIL_HOST_USER = 'tthieu281221@gmail.com'
-EMAIL_HOST_PASSWORD = 'jdsutipeikldmsnx'
+# Use environment variable for email password
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
